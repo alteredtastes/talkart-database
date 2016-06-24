ffa23c0a896c9d82ecf09dc9610662b14b58903e
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-06-23 22:50:44 -0700

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+require('dotenv').load();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/talkart',
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds/production'
+    }
+  }
+};
